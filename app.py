ee0be5d7bd8885f05e86ac9d5ab6c2e9a8fb97f8
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2015-01-16 11:31:23 -0500

    lets do this

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import markdown
+import yaml
+import collections
+
+from flask import Flask
+from flask import render_template
+from flask import Markup
+from flask import url_for
+from flask import abort
+from flask import request
+from flask.ext.frozen import Freezer
+from werkzeug import cached_property
+from werkzeug.contrib.atom import AtomFeed
+
+
+POST_FILE_EXTENSION   = '.md'
+POST_FILES_DIR        = 'posts'
+SECRET_KEY            = 'not-so-secret'
+FREEZER_BASE_URL      = 'http://localhost'
+FREEZER_IGNORED_FILES = ['.git', 'CNAME']
+
+
+app     = Flask(__name__)
+app.config.from_object(__name__)
+freezer = Freezer(app)
+
+app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
